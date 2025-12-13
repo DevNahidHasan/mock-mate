afe088b75a91a16c8d66991321d6075847f7cbbe
nirob08 <nahidnirob04@gmail.com>
2025-12-13 15:35:51 +0600

    chagned by nirob

--- public/index.php
+++ public/index.php
@@ -17,4 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleReques(Request::capture());
+$app->handleRequest(Request::capture());
